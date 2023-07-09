f419afd5f19c363e26d69dc6013c260623108cef
Jair Gomez <jairherber505@gmail.com>
2023-07-09 00:21:51 -0700

    added a component state

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
+
+
 function ListGroup() {
-  const items = ["New York", "San Francisco", "San Diego", "Colorado", "Tokyo"];
+  let items = ["New York", "San Francisco", "San Diego", "Colorado", "Tokyo"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => { setSelectedIndex(index)}}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>

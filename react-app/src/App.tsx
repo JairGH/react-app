b7e6b1107db230e7c420b32da31f4d16ef22050a
Jair Gomez <jairherber505@gmail.com>
2023-07-13 11:32:52 -0700

    ListGroup component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,10 @@
 import ListGroup from "./components/ListGroup";
 
+
+  
 function App() {
-  return <div><ListGroup /></div>
+  let items = ["New York", "San Francisco", "San Diego", "Colorado", "Tokyo"];
+  return <div><ListGroup items={items} heading="Cities"/></div>
 }
 
 export default App;
